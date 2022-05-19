be284621d1aa5e30c6e3d07ebb27679327a3939b
goodboop86 <goodboop86@gmail.com>
2022-05-20 01:29:42 +0900

    add factory

--- App/fastapi/crawler/crawler_factory.py
+++ App/fastapi/crawler/crawler_factory.py
@@ -1,10 +1,20 @@
 from enum import Enum
+from typing import Union
+
+from pydantic import HttpUrl
 from crawler.baseshop_crawler import BaseShopCrawler
 from model import crawler_model
+from datamodel.request_model import CrawlRequest
 
 
 class CrawlerFactory(object):
-    def __init__(self, crawl_domain, crawl_type) -> None:
-        self.__crawlers = {
-            ""
-        }
+    def __init__(self, request: CrawlRequest) -> None:
+        target: Union[list[HttpUrl], HttpUrl] = request.target
+        crawl_domain: str = request.strategy.crawl_domain
+        crawl_type: str = request.strategy.crawl_type
+
+        self.__cls = globals(crawl_domain)
+        self.__func = getattr(self.__cls, crawl_type)
+        self.__cls.__init__(target=target, func=self.__func)
+
+        return self.__cls
