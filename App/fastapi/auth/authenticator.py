8130d591a3bb0e2924051f30b670340215c5e285
goodboop86 <goodboop86@gmail.com>
2022-06-04 18:48:05 +0900

    phase4

--- App/fastapi/auth/authenticator.py
+++ App/fastapi/auth/authenticator.py
@@ -6,25 +6,12 @@ from jose import JWTError, jwt
 from datetime import datetime, timedelta
 from typing import Union
 
-fake_users_db = {
-    "johndoe": {
-        "username": "johndoe",
-        "full_name": "John Doe",
-        "email": "johndoe@example.com",
-        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
-        "disabled": False,
-    }
-}
-
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
 
 class Authenticator:
     pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+    ACCESS_TOKEN_EXPIRE_MINUTES = 30
+    ALGORITHM = "HS256"
+    SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 
     def __init__(self) -> None:
         pass
@@ -52,34 +39,14 @@ class Authenticator:
             user_dict = db[username]
             return UserInDB(**user_dict)
 
-    @staticmethod
-    async def get_current_user(token: str = Depends(oauth2_scheme)):
-        credentials_exception = HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Could not validate credentials",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-        try:
-            payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-            username: str = payload.get("sub")
-            if username is None:
-                raise credentials_exception
-            token_data = TokenData(username=username)
-        except JWTError:
-            raise credentials_exception
-        auth = Authenticator()
-        user = auth.get_user(fake_users_db, username=token_data.username)
-        if user is None:
-            raise credentials_exception
-        return user
-
-    @staticmethod
-    def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
+    @classmethod
+    def create_access_token(cls, data: dict, expires_delta: Union[timedelta, None] = None):
         to_encode = data.copy()
         if expires_delta:
             expire = datetime.utcnow() + expires_delta
         else:
             expire = datetime.utcnow() + timedelta(minutes=15)
         to_encode.update({"exp": expire})
-        encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+        encoded_jwt = jwt.encode(
+            to_encode, cls.SECRET_KEY, algorithm=cls.ALGORITHM)
         return encoded_jwt
--- App/fastapi/back.py
+++ App/fastapi/back.py
@@ -22,9 +22,6 @@ from pydantic import BaseModel
 from auth.authenticator import Authenticator
 from datamodel.auth_model import User, Token, TokenData, UserInDB
 
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 fake_users_db = {
     "johndoe": {
@@ -36,11 +33,34 @@ fake_users_db = {
     }
 }
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 app = FastAPI()
 
 
-async def get_current_active_user(current_user: User = Depends(Authenticator.get_current_user)):
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, Authenticator.SECRET_KEY,
+                             algorithms=[Authenticator.ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    auth = Authenticator()
+    user = auth.get_user(fake_users_db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
     if current_user.disabled:
         raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
@@ -57,7 +77,8 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token_expires = timedelta(
+        minutes=Authenticator.ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = auth.create_access_token(
         data={"sub": user.username}, expires_delta=access_token_expires
     )
